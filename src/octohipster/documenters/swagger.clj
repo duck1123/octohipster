a020aea36a79506a2cacced13a9843a88fdf3692
Daniel E. Renfer <duck@kronkltd.net>
2015-02-06 01:52:44 -0500

    update swagger to the next version

--- src/octohipster/documenters/swagger.clj
+++ src/octohipster/documenters/swagger.clj
@@ -1,55 +1,83 @@
 (ns octohipster.documenters.swagger
   (:use [octohipster core host mixins])
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [clojure.tools.logging :as log]))
 
 (def api-version "1.0")
-(def swagger-version "1.1")
+(def swagger-version "2.0")
 
-(defn swagger-root [groups]
-  {:apiVersion api-version
-   :swaggerVersion swagger-version
-   :basePath (str *host* *context*)
-   :apis (map (fn [g] {:path (:url g), :description (:desc g)}) groups)})
+(defn parameter-map
+  [resource method]
+  (if-let [params (concat (get resource :parameters [])
+                          (get-in resource [:methods (keyword method) :parameters] []))]
+    {:parameters
+     (map
+      (fn [[n m]]
+        (merge m {:name (name n)
+                  :required (:required m (= (:in m) "path"))}))
+      params)}))
 
-(defn swagger-root-doc [options]
+(defn swagger-operation
+  [g resource method]
+  (merge {:summary (get-in resource [:methods method :summary] "")
+          :tags (filter identity [(or (:name g) (:url g))])
+          :produces (:available-media-types resource)
+          :responses (or (get-in resource [:methods method :responses])
+                         {"200" {:description "Default Response"}})}
+         (parameter-map resource (name method))
+         (if-let [desc (or (get-in resource [:methods method :description])
+                           (:description resource))]
+           {:description desc})))
+
+(defn swagger-resource
+  [g resource]
+  (let [url (str (:url g) (:url resource))]
+    [[url
+      (merge
+       (when (:handle-ok resource)
+         {"get" (swagger-operation g resource :get)})
+       (when (:post! resource)
+         {"post" (merge {:summary (get-in resource [:methods :post :summary] "")
+                         :tags (filter identity [(or (:name g) (:url g))])
+                         :produces (:available-media-types resource)
+                         :responses (or (get-in resource [:methods :post :responses])
+                                        {"200" {:description "Default Response"}})}
+                        (parameter-map resource "post")
+                        (if-let [description (get-in resource [:methods :post :description])]
+                          {:description description}))})
+       (when (:delete! resource)
+         {"delete" (merge {:summary (get-in resource [:methods :delete :summary] "")
+                           :tags (filter identity [(or (:name g) (:url g))])
+                           :produces (:available-media-types resource)
+                           :responses {"200" {:description "Response"}}}
+                          (parameter-map resource "delete")
+                          (if-let [description (get-in resource [:methods :delete :description])]
+                            {:description description}))}))]]))
+
+(defn swagger-group
+  [g]
+  (->> (:resources g)
+       (sort-by :name)
+       (mapcat (partial swagger-resource g))))
+
+(defn swagger-root
+  "Generates a Swagger 2.0 API doc"
+  [ctx options]
+  {:swagger swagger-version
+   :info {:title (:name options)
+          :version api-version}
+   :schemes (:schemes options [(name (get-in ctx [:request :scheme] :http))])
+   :basePath (get-in ctx [:request :context] "/")
+   :host (get-in ctx [:request :headers "host"] "localhost")
+   :paths (->> (:groups options)
+               (sort-by :name)
+               (mapcat swagger-group)
+               (into (sorted-map)))})
+
+(defn swagger-doc
+  "Documenter Middleware"
+  [options]
   (resource
    {:url "/api-docs.json"
     :mixins [handled-resource]
-    :exists? (fn [ctx] {:data (swagger-root (:groups options))})}))
-
-(defn doc->operation [res [k v]]
-  (-> v
-      (assoc :httpMethod (string/upper-case (name k)))
-      (assoc :responseClass
-             (or (:responseClass v)
-                 (let [id (-> res :schema :id)]
-                   (if (and (= k :get) (:is-multiple? res))
-                     (str "Array[" id "]")
-                     id))))))
-
-(defn resource->api [group res]
-  {:path (str (:url group) (:url res))
-   :description (:desc res)
-   :operations (map (partial doc->operation res) (:doc res))})
-
-(defn resource->model [{:keys [schema]}]
-  [(keyword (:id schema)) schema])
-
-(defn swagger-api-decl [groups path]
-  (let [group (->> groups
-                   (filter #(= (:url %) path))
-                   first)
-        resources (:resources group)]
-    {:apiVersion api-version
-     :swaggerVersion swagger-version
-     :basePath (str *host* *context*)
-     :resourcePath (:url group)
-     :apis (map (partial resource->api group) resources)
-     :models (into {} (map resource->model resources))}))
-
-(defn swagger-doc [options]
-  (resource
-   {:url "/api-docs.json/{path}"
-    :mixins [handled-resource]
-    :exists? (fn [ctx] {:data (swagger-api-decl (:groups options)
-                                               (str "/" (-> ctx :request :route-params :path)))})}))
+    :exists? (fn [ctx] {:data (swagger-root ctx options)})}))
