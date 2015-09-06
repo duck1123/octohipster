1f49fd6d760550077110c1b239c3de5e2d9163e3
Daniel E. Renfer <duck@kronkltd.net>
2015-09-06 17:17:15 -0400

    swagger test

--- test/octohipster/documenters/swagger_test.clj
+++ test/octohipster/documenters/swagger_test.clj
@@ -60,42 +60,46 @@
 
 (facts "swagger-doc"
   (fact "exposes swagger api declarations"
-    (-> (request :get "/api-docs.json/contacts")
-        (header "Accept" "application/json")
-        site :body unjsonify) =>
-        {:apiVersion "1.0"
-         :swaggerVersion "1.1"
-         :basePath "http://localhost"
-         :resourcePath "/contacts"
-         :apis [{:path "/contacts"
-                 :description "Operations with multiple contacts"
-                 :operations [{:httpMethod "GET"
-                               :nickname "getContacts"
-                               :summary "Get all contacts"
-                               :responseClass "Array[Contact]"}
-                              {:httpMethod "POST"
-                               :nickname "createContact"
-                               :summary "Create a contact"
-                               :responseClass "Contact"
-                               :parameters [body-param]}]}
-                {:path "/contacts/{id}"
-                 :description "Operations with individual contacts"
-                 :operations [{:httpMethod "GET"
-                               :nickname "getContact"
-                               :summary "Get a contact"
-                               :responseClass "Contact"
-                               :parameters [name-param]}
-                              {:httpMethod "PUT"
-                               :nickname "updateContact"
-                               :summary "Update a contact"
-                               :responseClass "Contact"
-                               :parameters [name-param body-param]}
-                              {:httpMethod "DELETE"
-                               :nickname "deleteContact"
-                               :summary "Delete a contact"
-                               :responseClass "Contact"
-                               :parameters [name-param]}]}]
-         :models {:Contact contact-schema}}))
+    (let [req (-> (request :get "/api-docs.json/contacts")
+                  (header "Accept" "application/json"))]
+      (site req) =>
+      (contains
+       {:body (fn [body]
+                (fact
+                  (unjsonify body) =>
+                  {:apiVersion "1.0"
+                   :swaggerVersion "1.1"
+                   :basePath "http://localhost"
+                   :resourcePath "/contacts"
+                   :apis [{:path "/contacts"
+                           :description "Operations with multiple contacts"
+                           :operations [{:httpMethod "GET"
+                                         :nickname "getContacts"
+                                         :summary "Get all contacts"
+                                         :responseClass "Array[Contact]"}
+                                        {:httpMethod "POST"
+                                         :nickname "createContact"
+                                         :summary "Create a contact"
+                                         :responseClass "Contact"
+                                         :parameters [body-param]}]}
+                          {:path "/contacts/{id}"
+                           :description "Operations with individual contacts"
+                           :operations [{:httpMethod "GET"
+                                         :nickname "getContact"
+                                         :summary "Get a contact"
+                                         :responseClass "Contact"
+                                         :parameters [name-param]}
+                                        {:httpMethod "PUT"
+                                         :nickname "updateContact"
+                                         :summary "Update a contact"
+                                         :responseClass "Contact"
+                                         :parameters [name-param body-param]}
+                                        {:httpMethod "DELETE"
+                                         :nickname "deleteContact"
+                                         :summary "Delete a contact"
+                                         :responseClass "Contact"
+                                         :parameters [name-param]}]}]
+                   :models {:Contact contact-schema}}))}))))
 
 (facts "swagger-root-doc"
   (fact "exposes swagger resource listing at /api-docs.json"
